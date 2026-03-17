8fc5ace72b5acc74e68b304ab5e54df8421d2c91
Theo Shinigami <theo.cubero01102448@sesisenaipr.org.br>
2026-03-17 11:05:02 -0300

    Implement temperature condition checker
    Add a program to check temperature conditions.

---
+++ 16 03 26/exrev16.js
@@ -0,0 +1,10 @@
+//Criar um programa com uma condicional composta para verificar a temperatura entre "frio" "agradavel" ou "quente"
+let temperatura = 22;
+
+if (temperatura < 15) {
+    console.log("Frio");
+} else if (temperatura >= 15 && temperatura <= 25) {
+    console.log("Agradável");
+} else {
+    console.log("Quente");
+}
