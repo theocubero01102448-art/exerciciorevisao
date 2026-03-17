c976c7d539de9962d3ab675dd98afdf141143d3a
Theo Shinigami <theo.cubero01102448@sesisenaipr.org.br>
2026-03-17 11:10:27 -0300

    Correct comparison logic for number check
    Fix variable name and update comparison logic.

---
+++ 16 03 26/exrev11.js
@@ -0,0 +1,8 @@
+//criar um programa para mostrar seu um numero é maior que 22
+let num = 27
+let num2 = 22
+if (num1 >= num2){
+    console.log(`Número é Menor que 22`)
+} else {
+    console.log("Número é menor que 22")
+}
