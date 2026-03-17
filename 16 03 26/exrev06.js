d2957736e1686eec613449d7bf0adab695a5ea8d
Theo Shinigami <theo.cubero01102448@sesisenaipr.org.br>
2026-03-17 11:14:20 -0300

    Add division program for two numbers

--- 16 03 26/exrev06.js
+++ 16 03 26/exrev06.js
@@ -1,1 +1,5 @@
-//criar um programa que mostre a divisão entre dois números
+//criar um programa que mostre a divisao entre dois numeros
+let num1 = 2
+let num2 = 2
+let resultado = num1 / num2
+console.log(`a divisao de ${num1} / ${num2} é ${resultado}`)
