e46b3ed600a4547350c0fd0af31b941c98bc01a6
Theo Shinigami <theo.cubero01102448@sesisenaipr.org.br>
2026-03-17 11:14:49 -0300

    Implement multiplication program for two numbers

--- 16 03 26/exrev05.js
+++ 16 03 26/exrev05.js
@@ -1,1 +1,5 @@
 //criar um programa para mostrar a multiplicação entre dois numeros.
+let num1 = "5"
+let num2 = "10"
+let resultado = num1 * num2 
+console.log(`a multiplicação de ${num1} * ${num2} é ${multi}`)
