82e8d5fdeb2236ff4cca55431fd89753eb477d15
Theo Shinigami <theo.cubero01102448@sesisenaipr.org.br>
2026-03-17 11:09:59 -0300

    Add program to check if a number is a multiple of 5

--- 16 03 26/exrev12.js
+++ 16 03 26/exrev12.js
@@ -1,1 +1,7 @@
-//criar programa para verificar 
+// criar um programa para verificar se um numero é multiplo de 5
+let num1 = 25
+if (num1 % 5 == 0){
+console.log("O numero é mútiplo de 5")
+} else{
+    console.log("O número não de múltiplo de 5")
+}
