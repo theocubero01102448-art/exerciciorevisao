c0257228688764229ffc1860682e3bd560b33cda
Theo Shinigami <theo.cubero01102448@sesisenaipr.org.br>
2026-03-17 11:04:00 -0300

    Add age classification program

---
+++ 16 03 26/exrev18.js
@@ -0,0 +1,9 @@
+//criar um programa para classificar a faixa etaria entre "criança", "adolescente", ou "adulto"
+let idade = 25
+if (idade >= 0 && idade <= 12){
+    console.log("Criança")
+} else if (idade > 12 && idade <= 18){
+    console.log("Adolescente")
+} else {
+    console.log("Adulto")
+}
