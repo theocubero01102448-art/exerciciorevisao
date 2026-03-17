f275b530a67a95f4f444e71f4dd21ca6c49a1552
Theo Shinigami <theo.cubero01102448@sesisenaipr.org.br>
2026-03-17 11:05:28 -0300

    Add program to check approval based on grade

---
+++ 16 03 26/exrev15.js
@@ -0,0 +1,4 @@
+//Criar um programa para verificar se uma nota é maior ou igual a 60 representa "aprovado" ou "reprovado", usando operador terminário
+let nota = 70
+let resultado = (nota >= 60) ? "Aprovado" : "Reprovado"
+console.log(resultado);
