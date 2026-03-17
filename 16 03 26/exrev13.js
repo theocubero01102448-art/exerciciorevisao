6335bd5dd3cc374fa2816f0928db6e6013011b15
Theo Shinigami <theo.cubero01102448@sesisenaipr.org.br>
2026-03-17 11:07:25 -0300

    Add driving eligibility check using ternary operator

---
+++ 16 03 26/exrev13.js
@@ -0,0 +1,5 @@
+//Crie um programa que verifique se a pessoa pode dirigir, usando operador ternário.
+
+let idade = 18;
+let resultado = (idade >= 18) ? "Pode dirigir" : "Não pode dirigir";
+console.log(resultado);
